2572eb74783609499384b5bb36bc722f376326de
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-07 18:06:08 -0500

    added edge detection scores

--- tests/edge_detection.py
+++ tests/edge_detection.py
@@ -1,18 +1,8 @@
 import cv2
 import numpy as np
-
-'''
-# original image
-img = cv2.imread('images/n01917289_4982_real.png',0)
-
-# cyclegan image
-cimg = cv2.imread('images/n01917289_4982.png',0)
-
-# our image
-uimg = cv2.imread('images/n01917289_4982_gen_IG_0.0.png',0)
-'''
-
 import glob
+import ntpath
+from tqdm import tqdm
 
 # real images
 imgs = []
@@ -23,50 +13,56 @@ cimgs = []
 # our ugan images
 uimgs = []
 
-images = sorted(glob.glob('images/*.png'))
-for i in images:
-   img = cv2.imread(i)
+# all the image paths
+real_paths   = sorted(glob.glob('test_images/original/*.png'))
+cgan_paths   = sorted(glob.glob('test_images/cgan/*.png'))
+ugan_0_paths = sorted(glob.glob('test_images/ugan_0.0/*.png'))
+ugan_1_paths = sorted(glob.glob('test_images/ugan_1.0/*.png'))
 
-   if 'real' in i: imgs.append(img)
-   elif 'IG_0.0' in i: uimgs.append(img)
-   elif 'real' not in i and 'IG_1.0' not in i and 'IG_0.0' not in i: cimgs.append(img)
+# respective distances in image space of canny edge detection
+cgan_dist   = []
+ugan_0_dist = []
+ugan_1_dist = []
 
-imgs = np.asarray(imgs)
-cimgs = np.asarray(cimgs)
-uimgs = np.asarray(uimgs)
 
-for i,c,u in zip(imgs, cimgs, uimgs):
+# loop through all paths and open images
+for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
+
+   rimg  = cv2.imread(r)
+   cimg  = cv2.imread(c)
+   u0img = cv2.imread(u0)
+   u1img = cv2.imread(u1)
 
    # edges of original
-   edges = cv2.Canny(i,100,200)/255.0
+   edges = cv2.Canny(rimg,100,200)/255.0
 
    # edges of cyclegan
-   cedges = cv2.Canny(c,100,200)/255.0
+   cedges = cv2.Canny(cimg,100,200)/255.0
 
-   # edges of ugan
-   uedges = cv2.Canny(u,100,200)/255.0
+   # edges of ugan 0.0
+   u0edges = cv2.Canny(u0img,100,200)/255.0
 
-   print 'cyclegan: ',np.linalg.norm(edges-cedges)
-   print 'ugan:     ',np.linalg.norm(edges-uedges)
-   print
+   # edges of ugan 1.0
+   u1edges = cv2.Canny(u1img,100,200)/255.0
 
-   cv2.imwrite('img.png', i)
-   cv2.imwrite('edges.png', 255.0*edges)
-   cv2.imwrite('cedges.png', 255.0*cedges)
-   cv2.imwrite('uedges.png', 255.0*uedges)
+   cgan_dist.append(np.linalg.norm(edges-cedges))
+   ugan_0_dist.append(np.linalg.norm(edges-u0edges))
+   ugan_1_dist.append(np.linalg.norm(edges-u1edges))
 
-   exit()
+   #print 'cyclegan: ',np.linalg.norm(edges-cedges)
+   #print 'ugan_0:   ',np.linalg.norm(edges-u0edges)
+   #print 'ugan_1:   ',np.linalg.norm(edges-u1edges)
+   #print
 
-cv2.imwrite('edges.png', edges)
-cv2.imwrite('cedges.png', cedges)
-cv2.imwrite('uedges.png', uedges)
+   #cv2.imwrite('img.png', i)
+   #cv2.imwrite('edges.png', 255.0*edges)
+   #cv2.imwrite('cedges.png', 255.0*cedges)
+   #cv2.imwrite('uedges.png', 255.0*uedges)
 
-edges = edges/255.0
-cedges = cedges/255.0
-uedges = uedges/255.0
+print np.mean(cgan_dist)
+print np.mean(ugan_0_dist)
+print np.mean(ugan_1_dist)
 
-print np.linalg.norm(edges-cedges)
-print np.linalg.norm(edges-uedges)
-
+exit()
 
 
