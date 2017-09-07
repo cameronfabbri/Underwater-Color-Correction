adf636e6f3bb55264a246f3fa07a4533c86766ee
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-07 18:36:12 -0500

    update

--- tests/edge_detection.py
+++ tests/edge_detection.py
@@ -24,9 +24,14 @@ cgan_dist   = []
 ugan_0_dist = []
 ugan_1_dist = []
 
-
 # loop through all paths and open images
 for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
+
+   image_name = ntpath.basename(r)
+
+   #if image_name != 'n01496331_11938.png': continue
+   if image_name != 'n01917289_4078.png': continue
+
 
    rimg  = cv2.imread(r)
    cimg  = cv2.imread(c)
@@ -35,6 +40,7 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 
    # edges of original
    edges = cv2.Canny(rimg,100,200)/255.0
+
 
    # edges of cyclegan
    cedges = cv2.Canny(cimg,100,200)/255.0
@@ -48,11 +54,34 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
    cgan_dist.append(np.linalg.norm(edges-cedges))
    ugan_0_dist.append(np.linalg.norm(edges-u0edges))
    ugan_1_dist.append(np.linalg.norm(edges-u1edges))
+   
+   print 'cgan:   ',np.linalg.norm(edges-cedges)
+   print 'ugan_0: ',np.linalg.norm(edges-u0edges)
+   print 'ugan_1: ',np.linalg.norm(edges-u1edges)
+   print
 
-   #print 'cyclegan: ',np.linalg.norm(edges-cedges)
-   #print 'ugan_0:   ',np.linalg.norm(edges-u0edges)
-   #print 'ugan_1:   ',np.linalg.norm(edges-u1edges)
-   #print
+   # save a bunch of images for paper
+   # original images
+   cv2.imwrite('original.png', rimg)
+   cv2.imwrite('cimg.png', cimg)
+   cv2.imwrite('u0img.png', u0img)
+   cv2.imwrite('u1img.png', u1img)
+
+   # edges of original image
+   cv2.imwrite('edges.png', 255.0*edges)
+   
+   # edges of cgan
+   cv2.imwrite('cedges.png', 255.0*cedges)
+
+   # edges of ugan 0.0
+   cv2.imwrite('u0edges.png', 255.0*u0edges)
+   
+   # edges of ugan 1.0
+   cv2.imwrite('u1edges.png', 255.0*u1edges)
+
+
+   exit()
+
 
    #cv2.imwrite('img.png', i)
    #cv2.imwrite('edges.png', 255.0*edges)
@@ -62,6 +91,10 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 print np.mean(cgan_dist)
 print np.mean(ugan_0_dist)
 print np.mean(ugan_1_dist)
+# results
+# 105.447125252
+# 91.8003760136
+# 91.8462828424
 
 exit()
 
