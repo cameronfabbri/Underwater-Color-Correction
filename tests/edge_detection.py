bb21c5286458b4fca740bd284cb1ba4bc64ff99a
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-07 20:06:11 -0500

    update

--- tests/edge_detection.py
+++ tests/edge_detection.py
@@ -24,14 +24,25 @@ cgan_dist   = []
 ugan_0_dist = []
 ugan_1_dist = []
 
+save_images = ['n01496331_11763.png', 'n01496331_22079.png', 'n01496331_2629.png', 'n01914609_1607.png', 'n01664065_9210.png', 'n02607072_763.png']
+
+largest = 0
+l_img = ''
+
+save = 0
+
+# just open and close quick to wipe it
+results_file = open('result_images/results.txt', 'w')
+results_file.close()
+results_file = open('result_images/results.txt', 'a')
+results_file.write('image_name,cgan_dist,ugan_0_dist,ugan_1_dist\n')
+
 # loop through all paths and open images
 for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 
+   if save == 1 and ntpath.basename(r) not in save_images: continue
+   
    image_name = ntpath.basename(r)
-
-   #if image_name != 'n01496331_11938.png': continue
-   if image_name != 'n01917289_4078.png': continue
-
 
    rimg  = cv2.imread(r)
    cimg  = cv2.imread(c)
@@ -40,7 +51,6 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 
    # edges of original
    edges = cv2.Canny(rimg,100,200)/255.0
-
 
    # edges of cyclegan
    cedges = cv2.Canny(cimg,100,200)/255.0
@@ -51,42 +61,53 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
    # edges of ugan 1.0
    u1edges = cv2.Canny(u1img,100,200)/255.0
 
-   cgan_dist.append(np.linalg.norm(edges-cedges))
-   ugan_0_dist.append(np.linalg.norm(edges-u0edges))
-   ugan_1_dist.append(np.linalg.norm(edges-u1edges))
-   
-   print 'cgan:   ',np.linalg.norm(edges-cedges)
-   print 'ugan_0: ',np.linalg.norm(edges-u0edges)
-   print 'ugan_1: ',np.linalg.norm(edges-u1edges)
-   print
+   cgan_dist_   = np.linalg.norm(edges-cedges)
+   ugan_0_dist_ = np.linalg.norm(edges-u0edges)
+   ugan_1_dist_ = np.linalg.norm(edges-u1edges)
 
-   # save a bunch of images for paper
-   # original images
-   cv2.imwrite('original.png', rimg)
-   cv2.imwrite('cimg.png', cimg)
-   cv2.imwrite('u0img.png', u0img)
-   cv2.imwrite('u1img.png', u1img)
+   cgan_dist.append(cgan_dist_)
+   ugan_0_dist.append(ugan_0_dist_)
+   ugan_1_dist.append(ugan_1_dist_)
 
-   # edges of original image
-   cv2.imwrite('edges.png', 255.0*edges)
-   
-   # edges of cgan
-   cv2.imwrite('cedges.png', 255.0*cedges)
+   d = np.linalg.norm(edges-cedges) - np.linalg.norm(edges-u0edges)
 
-   # edges of ugan 0.0
-   cv2.imwrite('u0edges.png', 255.0*u0edges)
-   
-   # edges of ugan 1.0
-   cv2.imwrite('u1edges.png', 255.0*u1edges)
+   if d > largest:
+      largest = d
+      l_img = image_name
 
+   if save == 1:
 
-   exit()
+      print image_name
+      print 'cgan:   ',np.linalg.norm(edges-cedges)
+      print 'ugan_0: ',np.linalg.norm(edges-u0edges)
+      print 'ugan_1: ',np.linalg.norm(edges-u1edges)
+      print
 
+      # save a bunch of images for paper
+      # original images
+      cv2.imwrite('result_images/'+image_name+'_original.png', rimg)
+      cv2.imwrite('result_images/'+image_name+'_cimg.png', cimg)
+      cv2.imwrite('result_images/'+image_name+'_u0img.png', u0img)
+      cv2.imwrite('result_images/'+image_name+'_u1img.png', u1img)
 
-   #cv2.imwrite('img.png', i)
-   #cv2.imwrite('edges.png', 255.0*edges)
-   #cv2.imwrite('cedges.png', 255.0*cedges)
-   #cv2.imwrite('uedges.png', 255.0*uedges)
+      # edges of original image
+      cv2.imwrite('result_images/'+image_name+'_edges.png', 255.0*edges)
+      
+      # edges of cgan
+      cv2.imwrite('result_images/'+image_name+'_cedges.png', 255.0*cedges)
+
+      # edges of ugan 0.0
+      cv2.imwrite('result_images/'+image_name+'_u0edges.png', 255.0*u0edges)
+      
+      # edges of ugan 1.0
+      cv2.imwrite('result_images/'+image_name+'_u1edges.png', 255.0*u1edges)
+
+      results_file.write(image_name+','+str(cgan_dist_)+','+str(ugan_0_dist_)+','+str(ugan_1_dist_)+'\n')
+
+results_file.close()
+print 'done'
+#print l_img
+#print d
 
 print np.mean(cgan_dist)
 print np.mean(ugan_0_dist)
