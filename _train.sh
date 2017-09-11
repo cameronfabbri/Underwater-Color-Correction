cd7199f05d4647267fabfbc221b75ea3f28f5df0
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-10 20:30:08 -0500

    update

--- _train.sh
+++ _train.sh
@@ -1,1 +1,1 @@
-python train.py --DATA=rocks --EPOCHS=1000 --NETWORK=pix2pix --IG_WEIGHT=1.0 --L1_WEIGHT=100.0 --BATCH_SIZE=16 --LOSS_METHOD=wgan --LEARNING_RATE=1e-4
+python train.py --DATA=underwater_imagenet --EPOCHS=100 --NETWORK=pix2pix --IG_WEIGHT=0.0 --L1_WEIGHT=100.0 --BATCH_SIZE=32 --LOSS_METHOD=wgan --LEARNING_RATE=1e-4
