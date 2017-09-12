31f8b44f4fd629a763846de20bc5a712d62a057f
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-12 15:13:58 -0500

    update

---
+++ tests/ren.sh
@@ -0,0 +1,11 @@
+# eval.py spits out images called n*_gen.png and n*_real.png
+# This script renames them to scrap the real and gen in the test images folder
+
+
+rm test_images/ugan_0.0/*real*
+rm test_images/ugan_1.0/*real*
+
+rename 's/_gen//' test_images/ugan_0.0/*
+rename 's/_gen//' test_images/ugan_1.0/*
+
+
--- tests/zoom.py
+++ tests/zoom.py
@@ -9,9 +9,34 @@ import numpy as np
 import sys
 
 
-img = misc.imread(sys.argv[1])
+img = 'n01496331_15872.png'
 
-crop = img[200:250, 200:250, :]
+root = 'test_images/'
 
-misc.imsave('crop.png', crop)
+dirs = ['cgan/', 'original/', 'ugan_0.0/', 'ugan_1.0/']
 
+for d in dirs:
+   img = misc.imread(root+d+img)
+
+   y1 = 160
+   y2 = 200
+   x1 = 210
+   x2 = 250
+
+
+   # row, column (y, x), (0,0) at top left
+   crop = img[y1:y2, x1:x2, :]
+
+   crop = misc.imresize(crop, (64, 64))
+   misc.imsave('crop1.png', crop)
+   # draw box around cropped area
+   # left
+   img[y1:y2, x1, :] = [255, 0, 0]
+   # top
+   img[y1, x1:x2, :] = [255, 0, 0]
+   # bottom
+   img[y2, x1:x2, :] = [255, 0, 0]
+   # right
+   img[y1:y2, x2, :] = [255, 0, 0]
+
+   misc.imsave('og.png', img)
