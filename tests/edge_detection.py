4941287fde8567d590aa37df5aace825e8d5972b
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-14 12:13:41 -0500

    update

--- tests/edge_detection.py
+++ tests/edge_detection.py
@@ -14,10 +14,10 @@ cimgs = []
 uimgs = []
 
 # all the image paths
-real_paths   = sorted(glob.glob('test_images/original/*.png'))
+real_paths   = sorted(glob.glob('test_images/flickr/*.png'))
 cgan_paths   = sorted(glob.glob('test_images/cgan/*.png'))
-ugan_0_paths = sorted(glob.glob('test_images/ugan_0.0/*.png'))
-ugan_1_paths = sorted(glob.glob('test_images/ugan_1.0/*.png'))
+ugan_0_paths = sorted(glob.glob('test_images/ugan_fl_0.0/*.png'))
+ugan_1_paths = sorted(glob.glob('test_images/ugan_fl_1.0/*.png'))
 
 # respective distances in image space of canny edge detection
 cgan_dist   = []
@@ -25,17 +25,18 @@ ugan_0_dist = []
 ugan_1_dist = []
 
 #save_images = ['n02607072_4739.png', 'n01496331_15872.png', 'n01496331_22079.png']
-save_images = ['n01496331_15872.png']
 
-largest = 0
-l_img = ''
+# orange corral
+#save_images = ['5538756669_6a5627ea9c_o.png']
 
-save = 1
+save_images = ['525931976_dffc710a58_o.png', '8032289_2af635da3c_o.png', '1056733511_8a8c0d9410_o.png', '2508954302_63b17667ce_o.png']
+
+save = 0
 
 # just open and close quick to wipe it
-results_file = open('result_images/results.txt', 'w')
+results_file = open('edge_results/results.txt', 'w')
 results_file.close()
-results_file = open('result_images/results.txt', 'a')
+results_file = open('edge_results/results.txt', 'a')
 results_file.write('image_name,cgan_dist,ugan_0_dist,ugan_1_dist\n')
 
 # loop through all paths and open images
@@ -72,10 +73,6 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 
    d = np.linalg.norm(edges-cedges) - np.linalg.norm(edges-u0edges)
 
-   #if d > largest:
-   #   largest = d
-   #   l_img = image_name
-
    if save == 1:
 
       print image_name
@@ -86,37 +83,31 @@ for r,c,u0,u1 in tqdm(zip(real_paths, cgan_paths, ugan_0_paths, ugan_1_paths)):
 
       # save a bunch of images for paper
       # original images
-      cv2.imwrite('result_images/'+image_name+'_original.png', rimg)
-      cv2.imwrite('result_images/'+image_name+'_cimg.png', cimg)
-      cv2.imwrite('result_images/'+image_name+'_u0img.png', u0img)
-      cv2.imwrite('result_images/'+image_name+'_u1img.png', u1img)
+      cv2.imwrite('edge_results/'+image_name+'_original.png', rimg)
+      cv2.imwrite('edge_results/'+image_name+'_cimg.png', cimg)
+      cv2.imwrite('edge_results/'+image_name+'_u0img.png', u0img)
+      cv2.imwrite('edge_results/'+image_name+'_u1img.png', u1img)
 
       # edges of original image
-      cv2.imwrite('result_images/'+image_name+'_oedges.png', 255.0*edges)
+      cv2.imwrite('edge_results/'+image_name+'_oedges.png', 255.0*edges)
       
       # edges of cgan
-      cv2.imwrite('result_images/'+image_name+'_cedges.png', 255.0*cedges)
+      cv2.imwrite('edge_results/'+image_name+'_cedges.png', 255.0*cedges)
 
       # edges of ugan 0.0
-      cv2.imwrite('result_images/'+image_name+'_u0edges.png', 255.0*u0edges)
+      cv2.imwrite('edge_results/'+image_name+'_u0edges.png', 255.0*u0edges)
       
       # edges of ugan 1.0
-      cv2.imwrite('result_images/'+image_name+'_u1edges.png', 255.0*u1edges)
+      cv2.imwrite('edge_results/'+image_name+'_u1edges.png', 255.0*u1edges)
 
       results_file.write(image_name+','+str(cgan_dist_)+','+str(ugan_0_dist_)+','+str(ugan_1_dist_)+'\n')
 
 results_file.close()
 print 'done'
-#print l_img
-#print d
 
 print np.mean(cgan_dist)
 print np.mean(ugan_0_dist)
 print np.mean(ugan_1_dist)
-# results
-# 105.447125252
-# 91.8003760136
-# 91.8462828424
 
 exit()
 
