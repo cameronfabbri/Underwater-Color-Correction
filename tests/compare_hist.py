f004a47e893986c214fcb0474b20b8f516e59d78
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-15 21:30:21 -0500

    update

--- tests/compare_hist.py
+++ tests/compare_hist.py
@@ -54,42 +54,50 @@ for i in range(1,5):
    ugan   = ugan/255.0
    uganp  = uganp/255.0
 
+   '''
    print '+--------+'
    print '|  crop'+str(i)+' |'
    print '+--------+'
    print 
-   print 'gdl flickr-cgan   :',gdl(flickr, cgan)
-   print 'gdl flickr-ugan   :',gdl(flickr, ugan)
-   print 'gdl flickr-uganp  :',gdl(flickr, uganp)
+   print 'gdl flickr-cgan    :',gdl(flickr, cgan)
+   print 'gdl flickr-ugan    :',gdl(flickr, ugan)
+   print 'gdl flickr-uganp   :',gdl(flickr, uganp)
    print
-   print 'flickr_mean       :',np.mean(flickr)
-   print 'cgan_mean         :',np.mean(cgan)
-   print 'ugan_mean         :',np.mean(ugan)
-   print 'uganp_mean        :',np.mean(uganp)
+   print 'flickr_mean        :',np.mean(flickr)
+   print 'cgan_mean          :',np.mean(cgan)
+   print 'ugan_mean          :',np.mean(ugan)
+   print 'uganp_mean         :',np.mean(uganp)
    print
-   print 'flickr_var        :',np.var(flickr)
-   print 'cgan_var          :',np.var(cgan)
-   print 'ugan_var          :',np.var(ugan)
-   print 'uganp_var         :',np.var(uganp)
+   print 'flickr_std         :',np.std(flickr)
+   print 'cgan_std           :',np.std(cgan)
+   print 'ugan_std           :',np.std(ugan)
+   print 'uganp_std          :',np.std(uganp)
    print
-   print 'flickr_cgan bhatt :',a
-   print 'flickr_ugan bhatt :',b
-   print 'flickr_uganp bhatt:',c
+   print 'flickr_var         :',np.var(flickr)
+   print 'cgan_var           :',np.var(cgan)
+   print 'ugan_var           :',np.var(ugan)
+   print 'uganp_var          :',np.var(uganp)
    print
-   print 'snr flickr        :',signaltonoise(flickr, axis=None)
-   print 'snr cgan          :',signaltonoise(cgan, axis=None)
-   print 'snr ugan          :',signaltonoise(ugan, axis=None)
-   print 'snr uganp         :',signaltonoise(uganp, axis=None)
+   print 'snr flickr         :',signaltonoise(flickr, axis=None)
+   print 'snr cgan           :',signaltonoise(cgan, axis=None)
+   print 'snr ugan           :',signaltonoise(ugan, axis=None)
+   print 'snr uganp          :',signaltonoise(uganp, axis=None)
    print
-   print 'cgan:             ',psnr(flickr,cgan)
-   print 'ugan:             ',psnr(flickr,ugan)
-   print 'uganp:            ',psnr(flickr,uganp)
+   print 'psnr cgan          :',psnr(flickr,cgan)
+   print 'psnr ugan          :',psnr(flickr,ugan)
+   print 'psnr uganp         :',psnr(flickr,uganp)
    print
-   print 'laplac flickr     :',cv2.Laplacian(flickr, cv2.CV_64F).var()
-   print 'laplac cgan       :',cv2.Laplacian(cgan, cv2.CV_64F).var()
-   print 'laplac ugan       :',cv2.Laplacian(ugan, cv2.CV_64F).var()
-   print 'laplac uganp      :',cv2.Laplacian(uganp, cv2.CV_64F).var()
+   print 'laplac flickr      :',cv2.Laplacian(flickr, cv2.CV_64F).var()
+   print 'laplac cgan        :',cv2.Laplacian(cgan, cv2.CV_64F).var()
+   print 'laplac ugan        :',cv2.Laplacian(ugan, cv2.CV_64F).var()
+   print 'laplac uganp       :',cv2.Laplacian(uganp, cv2.CV_64F).var()
+   print
+   print 'flickr_cgan bhatt  :',a
+   print 'flickr_ugan bhatt  :',b
+   print 'flickr_uganp bhatt :',c
    print 
    print '----------------------------------------------'
    print
+   '''
+   print '& ' + str(round(np.mean(flickr), 2)) + ' $\pm$ ' + str(round(np.std(flickr), 2)) + ' & ' + str(round(np.mean(cgan), 2)) + ' $\pm$ ' + str(round(np.std(cgan), 2)) + ' & ' + str(round(np.mean(ugan), 2)) + ' $\pm$ ' + str(round(np.std(ugan), 2)) + ' & ' + str(round(np.mean(uganp), 2)) + ' $\pm$ ' + str(round(np.std(uganp), 2)) + ' \\\ \hline'
 
