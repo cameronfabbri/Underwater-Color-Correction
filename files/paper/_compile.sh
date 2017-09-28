bd9847c3319103e752d7bf1f474f5e2d31e4280b
Cameron Fabbri <cameronfabbri@gmail.com>
2017-09-28 14:28:06 -0500

    update

--- files/paper/_compile.sh
+++ files/paper/_compile.sh
@@ -3,3 +3,11 @@ pdflatex paper
 bibtex paper
 bibtex paper
 pdflatex paper
+
+# arxiv version
+rm PlainPaper.aux PlainPaper.b* PlainPaper.log PlainPaper.pdf
+pdflatex PlainPaper
+bibtex PlainPaper
+bibtex PlainPaper
+pdflatex PlainPaper
+
