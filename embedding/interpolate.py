3c58658b529b345a0e43e6af4f70b3d47a806ac5
Cameron Fabbri <cameronfabbri@gmail.com>
2018-04-20 16:56:13 -0500

    update

--- embedding/interpolate.py
+++ embedding/interpolate.py
@@ -1,8 +1,10 @@
 import numpy as np
 import scipy.misc as misc 
       
-x1 = misc.imread('clean/turtle1.jpg')
-x2 = misc.imread('distorted/turtle1.jpg')
+#x1 = misc.imread('clean/turtle1.jpg')
+#x2 = misc.imread('distorted/turtle1.jpg')
+x1 = misc.imread('n01496331_1692_real.png')
+x2 = misc.imread('n01496331_1692_gen.png')
 
 num_int = 10
 
@@ -13,7 +15,15 @@ for a in alpha:
    latent_vectors.append(vector)
 latent_vectors = np.asarray(latent_vectors)
 
+canvas = 255*np.ones((256, (num_int)*256, 3), dtype=np.uint8)
+
 i = 0
+start=0
+end=256
 for e in latent_vectors:
-   misc.imsave(str(i)+'.png', e)
-   i += 1
+   #misc.imsave(str(i)+'.png', e)
+   #i += 1
+   canvas[:,start:end,:] = e
+   start=end
+   end = start+256
+misc.imsave('canvas.png', canvas)
